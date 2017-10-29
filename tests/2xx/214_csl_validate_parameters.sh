60a9e295351783b94d1d55f3994a28a99c9c4c97
Andreas Unterkircher <unki@netshadow.net>
2017-10-29 15:34:17 +0100

    2xx/214_csl_validate_parameters.sh, test with more complex thresholds too

--- tests/2xx/214_csl_validate_parameters.sh
+++ tests/2xx/214_csl_validate_parameters.sh
@@ -14,5 +14,9 @@ assert_func csl_validate_parameters $TEST_OK $TEST_EMPTY
 #CSL_CRITICAL_THRESHOLD=x
 #assert_func csl_validate_parameters $TEST_FAIL "critical parameter contains an invalid value"
 
+CSL_WARNING_THRESHOLD='/^downstream.*_power/=-1:1,/^downstream.*snr/=35:,/^upstream.*_power/=30:'
+CSL_CRITICAL_THRESHOLD='/^downstream.*_power/=-0.5:0.5,/^downstream.*snr/=35:,/^upstream.*_power/=35:'
+assert_func csl_validate_parameters $TEST_OK $TEST_EMPTY
+
 unset -v CSL_WARNING_THRESHOLD CSL_CRITICAL_THRESHOLD
 assert_func csl_validate_parameters $TEST_FAIL "are mandatory"
