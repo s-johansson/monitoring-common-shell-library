d5d9fe88aebdd8cf6cb10d00ffa2b4bac3485bbe
Andreas Unterkircher <unki@netshadow.net>
2017-10-29 10:13:52 +0100

    352_eval_results.sh, check if result-values with unit-of-measure get handled correctly

--- tests/3xx/352_eval_results.sh
+++ tests/3xx/352_eval_results.sh
@@ -7,8 +7,8 @@ declare -g CSL_EXIT_NO_DATA_IS_CRITICAL=false
 assert_func eval_results "${TEST_OK}" "${TEST_EMPTY}"
 CSL_EXIT_NO_DATA_IS_CRITICAL=true
 
-#
-# remember: we check here if the function eval_results() returns
+
+## remember: we check here if the function eval_results() returns
 # correctly as desired. we do not check if the plugin-output, -code
 # or perfdata have been set.
 #
@@ -20,14 +20,36 @@ assert_func eval_results "${TEST_OK}" "${TEST_EMPTY}"
 CSL_RESULT_VALUES+=( ['abc']='123' )
 assert_func eval_results "${TEST_FAIL}" "No[[:blank:]]plugin[[:blank:]]results[[:blank:]]have[[:blank:]]been[[:blank:]]evaluated"
 CSL_RESULT_VALUES+=( ['abc']='123' )
-declare -g CSL_WARNING_THRESHOLD=(
+declare -g -A CSL_WARNING_THRESHOLD=(
    ['abc']='5:'
 )
 assert_func eval_results "${TEST_FAIL}" "Unable[[:blank:]]to[[:blank:]]retrieve[[:blank:]]thresholds"
-declare -g CSL_CRITICAL_THRESHOLD=(
+declare -g -A CSL_CRITICAL_THRESHOLD=(
    ['abc']='5:'
 )
 assert_func eval_results "${TEST_OK}" "${TEST_EMPTY}"
 CSL_RESULT_VALUES=()
+assert_func eval_results "${TEST_OK}" "${TEST_EMPTY}"
+
+declare -g -A CSL_RESULT_VALUES=(
+   ['abc']='123dBmV'
+   ['def']='12.34dBmV'
+   ['ghi']='123 dBmV'
+   ['jkl']='12.34 dBmV'
+)
+
+declare -g -A CSL_WARNING_THRESHOLD=(
+   ['abc']='50:'
+   ['def']=':5'
+   ['ghi']=':50'
+   ['jkl']=':5'
+)
+declare -g -A CSL_CRITICAL_THRESHOLD=(
+   ['abc']=':100'
+   ['def']=':10'
+   ['ghi']=':100'
+   ['jkl']=':10'
+)
+
 assert_func eval_results "${TEST_OK}" "${TEST_EMPTY}"
 unset -v CSL_RESULT_VALUES CSL_WARNING_THRESHOLD CSL_CRITICAL_THRESHOLD
