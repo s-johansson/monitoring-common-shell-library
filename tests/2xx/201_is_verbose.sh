cd564d2e0acf770959a5af48866167045f819f4e
Andreas Unterkircher <unki@netshadow.net>
2018-12-08 07:57:32 +0100

    tests/2xx/201_is_verbose.sh, use boolean

--- tests/2xx/201_is_verbose.sh
+++ tests/2xx/201_is_verbose.sh
@@ -2,8 +2,9 @@
 
 . common.sh
 
-unset -v CSL_VERBOSE
+CSL_VERBOSE=false
 assert_func is_verbose $TEST_FALSE $TEST_EMPTY
-CSL_VERBOSE=1
+CSL_VERBOSE=true
 assert_func is_verbose $TEST_TRUE $TEST_EMPTY
-unset -v CSL_VERBOSE
+CSL_VERBOSE=false
+assert_func is_verbose $TEST_FALSE $TEST_EMPTY
